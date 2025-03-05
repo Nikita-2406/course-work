3c93d8bd46c2a34988de49d320b6515b3d78c278
Nikita-2406 <89524208686@mail.ru>
2025-03-05 14:29:21 +0300

    re

--- frontend/src/UI/settings.js
+++ frontend/src/UI/settings.js
@@ -1,2 +1,2 @@
-export const BASEURLWEBSITE = "http://193.227.240.83:5173/"
-export const BASEUSLAPI = 'http://193.227.240.83:8000/'
+export const BASEURLWEBSITE = "http://localhost:5173/"
+export const BASEUSLAPI = 'http://127.0.0.1:8000/'
